6e59ed1e7f4851f92c543bf9a6c2febc75743a1b
Dougal Stewart <dougal.koji@gmail.com>
2020-03-06 13:12:33 +0900

    highlighted navigation bar items depending on view

--- navBar.js
+++ navBar.js
@@ -47,3 +47,32 @@ function toggleMenubar() {
 	if (navBarRight.style.display == "") navBarRight.style.display = "flex";
 	else navBarRight.style.display = null;
 }
+
+// Highlight correct navbar button
+
+function isVisible(element) {
+	let top = 0;
+	let iterator = element;
+	while (!!iterator && iterator.tagName.toLowerCase() !== "body") {
+		top += iterator.offsetTop;
+		iterator = iterator.offsetParent;
+	}
+
+	return top-0.25*window.innerHeight <= document.documentElement.scrollTop;
+}
+
+function highlightNavBarElem() {
+	for (let i = navBarRight.children.length-1; i >= 0; --i) {
+		let targetID = navBarRight.children[i].getAttribute("href").substr(1);
+		let targetElem = document.getElementById(targetID);
+
+		if (isVisible(targetElem)) {
+			for (let j in navBarRight.children) navBarRight.children[j].id = null;
+			navBarRight.children[i].id = "navBar-highlighted";
+			break;
+		}
+	}
+}
+
+highlightNavBarElem();
+window.addEventListener('scroll', highlightNavBarElem);
