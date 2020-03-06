e62fb92ab7de0d014d57a0efc8d9d49e4e221e1f
Dougal Stewart <dougal.koji@gmail.com>
2020-03-06 20:36:25 +0900

    simplified soundcloudPlayer.js

--- soundcloudPlayer.js
+++ soundcloudPlayer.js
@@ -1,30 +1,30 @@
 var tracks = document.getElementsByClassName("track");
-for (let i = 0; i < tracks.length; ++i) {
-	let player = SC.Widget(tracks[i].children[0]);
+for (let track of tracks) {
+	let player = SC.Widget(track.children[0]);
 
 	// Set info
-	player.bind(SC.Widget.Events.READY, function(e) {
+	player.bind(SC.Widget.Events.READY, function() {
 		player.getCurrentSound(function(sound) {
 			var artworkUrl = sound.artwork_url || 'Fallback-Cover-Art.png';
-			tracks[i].children[1].children[2].href = sound.permalink_url;
-			tracks[i].children[2].children[0].href = sound.permalink_url;
-			tracks[i].children[2].children[0].innerText = sound.title;
-			tracks[i].children[1].children[1].setAttribute('style', "background-image: url("+artworkUrl+")");
+			track.children[1].children[2].href = sound.permalink_url;
+			track.children[2].children[0].href = sound.permalink_url;
+			track.children[2].children[0].innerText = sound.title;
+			track.children[1].children[1].style.backgroundImage= "url("+artworkUrl+")";
 		});
 	});
 
 	// Add event listeners for multimedia controls
-	tracks[i].children[1].children[0].addEventListener("click", function(){
+	track.children[1].children[0].addEventListener("click", function(){
 		player.toggle();
-		if (tracks[i].children[1].children[0].src.substr(-9) === "Pause.svg")
-			tracks[i].children[1].children[0].src = "/icons/Play.svg";
+		if (track.children[1].children[0].src.substr(-9) === "Pause.svg")
+			track.children[1].children[0].src = "/icons/Play.svg";
 		else
-			tracks[i].children[1].children[0].src = "/icons/Pause.svg";
+			track.children[1].children[0].src = "/icons/Pause.svg";
 	});
 
 	// Reset widget if the track finishes
-	player.bind(SC.Widget.Events.FINISH, function(e) {
-		tracks[i].children[1].children[0].src = "/icons/Play.svg";
+	player.bind(SC.Widget.Events.FINISH, function() {
+		track.children[1].children[0].src = "/icons/Play.svg";
 		player.seekTo(0);
 	})
 }
